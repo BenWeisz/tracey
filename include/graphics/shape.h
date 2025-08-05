84395c1dd7837ca9e2ecc69c5d0e3fa02b26fb2a
Ben Weisz <bencew99@gmail.com>
2025-08-04 22:54:05 -0700

    Add shapes

---
+++ include/graphics/shape.h
@@ -0,0 +1,42 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "types.h"
+#include "math/vec.h"
+#include "graphics/ray.h"
+#include "util/glist.h"
+
+#define SHAPE_TYPE_SPHERE 0x00
+
+typedef struct SHAPE
+{
+    u32 type;
+    void* form;
+} SHAPE;
+
+
+typedef struct SHAPE_HIT
+{
+    VEC4 p;
+    VEC4 n;
+    f64 t;
+} SHAPE_HIT;
+
+SHAPE* SHAPE_create(const u32 type);
+void SHAPE_destroy(SHAPE* shape);
+bool SHAPE_hit(const SHAPE* shape, const RAY* ray, const f64 t_min, const f64 t_max, SHAPE_HIT* shape_hit);
+
+typedef struct SPHERE
+{
+    VEC4 c;
+    f64 r;
+} SPHERE;
+
+bool SPHERE_hit(const SPHERE* sphere, const RAY* ray, const f64 t_min, const f64 t_max, SHAPE_HIT* shape_hit);
+
+GLIST_DECLARE(SHAPE, SHAPE_list)
+
+#endif // SHAPE_H
---
+++ src/graphics/shape.c
@@ -0,0 +1,86 @@
+#include "graphics/shape.h"
+
+GLIST_define(SHAPE, SHAPE_list)
+
+SHAPE* SHAPE_create(const u32 type)
+{
+    SHAPE* shape = (SHAPE*)malloc(sizeof(SHAPE));
+    if (shape == NULL)
+    {
+        printf("ERROR: Failed to allocate memory for shape.\n");
+        return NULL;
+    }
+
+    shape->type = type;
+    shape->form = NULL;
+
+    if (type == SHAPE_TYPE_SPHERE)
+    {
+        shape->form = malloc(sizeof(SPHERE));
+        if (shape->form == NULL)
+        {
+            printf("ERROR: Failed to allocate memory for the sphere.\n");
+            free(shape);
+            return NULL;
+        }
+
+        SPHERE* sphere = (SPHERE*)(shape->form);
+        sphere->c = VEC4_zero();
+        sphere->r = 0.0;
+    }
+    else
+    {
+        printf("ERROR: The form provided does not specify a valid shape.\n");
+        free(shape);
+        return NULL;
+    }
+
+    return shape;
+}
+
+void SHAPE_destroy(SHAPE* shape)
+{
+    free(shape->form);
+    free(shape);
+}
+
+bool SHAPE_hit(const SHAPE* shape, const RAY* ray, const f64 t_min, const f64 t_max, SHAPE_HIT* shape_hit)
+{
+    if (shape->type == SHAPE_TYPE_SPHERE)
+    {
+        return SPHERE_hit((SPHERE*)(shape->form), ray, t_min, t_max, shape_hit);
+    }
+
+    return false;
+}
+
+bool SPHERE_hit(const SPHERE* sphere, const RAY* ray, const f64 t_min, const f64 t_max, SHAPE_HIT* shape_hit)
+{
+    if (sphere->r <= 1e-6)
+    {
+        printf("ERROR: Did you forget to initialize this sphere?\n");
+        return false;
+    }
+
+    VEC4 oc = VEC4_sub(sphere->c, ray.origin); // o to c
+    f64 a = VEC4_dot3(ray.dir, ray.dir);
+    f64 h = VEC4_dot3(ray.dir, oc);
+    f64 c = VEC4_dot3(oc, oc) - (sphere->r * sphere->r);
+    f64 discriminant = (h * h) - (a * sphere->c);
+
+    if (discriminant < 0) return false;
+
+    f64 sqrtd = sqrt(discriminant);
+    shape_hit->t = (h - sqrtd) / a;
+
+    if (shape_hit->t < t_min || shape_hit->t > t_max)
+    {
+        shape_hit->t = (h + sqrtd) / a;
+        if (shape_hit->t < t_min || shape_hit->t > t_max)
+            return false;
+    }
+
+    shape_hit->p = RAY_at(*ray, shape_hit->t);
+    shape_hit->n = VEC4_div(VEC4_sub(shape_hit->p, sphere->c), sphere->r);
+    return true;
+}
